33155f76f8f207c7d9fce96c369fc4c45a1a93a6
Michael Ala <michaelfonzolo@gmail.com>
2015-11-04 14:07:11 -0500

    Updated Reflection.cs

--- Artemis.Engine/Utilities/Reflection.cs
+++ Artemis.Engine/Utilities/Reflection.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Artemis.Engine.Utilities
 {
-    internal class Reflection
+
+    /// <summary>
+    /// A static class containing convenience methods for certain reflection
+    /// related operations, such as retrieving attributes from an object.
+    /// </summary>
+    internal static class Reflection
     {
 
+        /// <summary>
+        /// Return the first Attribute of the given type applied to the give object.
+        /// </summary>
+        /// <param name="type">The object from which to retrieve the attribute.</param>
+        /// <param name="attr">The attribute type.</param>
+        /// <returns></returns>
     	public static Attribute GetFirstAttribute(Type type, Type attr)
     	{
-    		foreach (var other_attr in System.Attribute.GetCustomAttributes(type))
+    		foreach (var other_attr in Attribute.GetCustomAttributes(type))
     		{
     			if (other_attr.GetType() == attr)
     			{
@@ -20,10 +29,16 @@ namespace Artemis.Engine.Utilities
     		return null;
     	}
 
+        /// <summary>
+        /// Return a list of the attributes of the given type applied to the given object.
+        /// </summary>
+        /// <param name="type">The object from which to retrieve the attributes.</param>
+        /// <param name="attr">The attribute type.</param>
+        /// <returns></returns>
     	public static List<Attribute> GetAttributes(Type type, Type attr)
     	{
     		var attrs = new List<Attribute>();
-    		foreach (var other_attr in System.Attribute.GetCustomAttributes(type))
+    		foreach (var other_attr in Attribute.GetCustomAttributes(type))
     		{
     			if (other_attr.GetType() == attr)
     			{
@@ -33,10 +48,16 @@ namespace Artemis.Engine.Utilities
     		return attrs;
     	}
 
+        /// <summary>
+        /// Return the first Attribute of the given type applied to the given object.
+        /// </summary>
+        /// <typeparam name="T">The attribute type.</typeparam>
+        /// <param name="type">The object from which to retrieve the attribute.</param>
+        /// <returns></returns>
     	public static T GetFirstAttribute<T>(Type type) where T : Attribute
     	{
     		var attr = typeof(T);
-    		foreach (var other_attr in System.Attribute.GetCustomAttributes(type))
+    		foreach (var other_attr in Attribute.GetCustomAttributes(type))
     		{
     			if (other_attr.GetType() == attr)
     			{
@@ -46,10 +67,17 @@ namespace Artemis.Engine.Utilities
     		return null;
     	}
 
+        /// <summary>
+        /// Return a list of attributes of the given type applied to the given object.
+        /// </summary>
+        /// <typeparam name="T">The attribute type.</typeparam>
+        /// <param name="type">The object from which to retrieve the attributes.</param>
+        /// <returns></returns>
     	public static List<T> GetAttributes<T>(Type type) where T : Attribute
     	{
+            var attr = typeof(T);
     		var attrs = new List<T>();
-    		foreach (var other_attr in System.Attributes.GetCustomAttributes(type))
+    		foreach (var other_attr in Attribute.GetCustomAttributes(type))
     		{
     			if (other_attr.GetType() == attr)
     			{
