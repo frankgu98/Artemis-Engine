ce499c57059c66e48fd4cc430b49f86d4c68251c
Michael Ala <michaelfonzolo@gmail.com>
2015-11-04 12:20:36 -0500

    Barebones AssetLoader completed.

---
+++ Artemis.Engine/AssetLoader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Artemis.Engine
+{
+    public static class AssetLoader
+    {
+
+    	private static ContentManager Content;
+
+    	internal static void Initialize(ContentManager content)
+    	{
+    		AssetLoader.Content = content;
+    	}
+
+    	public static Texture2D Texture(string name)
+    	{
+    		return Content.Load<Texture2D>(name);
+    	}
+
+    	public static SpriteFont Font(string name)
+    	{
+    		return Content.Load<SpriteFont>(name);
+    	}
+
+    }
+}
