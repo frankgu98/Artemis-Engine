c429d392fa7a7e64cc1e19915c70c507545b5360
Frank Gu <frankgu98@gmail.com>
2015-11-04 18:14:54 -0500

    doesn't build

---
+++ Artemis.Engine/RenderPipeline.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Artemis.Engine
+{
+    public class RenderPipeline
+    {
+        public GraphicsDevice GraphicsDevice { get; private set; }
+        public GraphicsDeviceManager GraphicsDeviceManager { get; private set; }
+
+        private SpriteBatch SpriteBatch;
+
+        public RenderPipeline(SpriteBatch sb, GraphicsDevice gd, GraphicsDeviceManager gdm)
+        {
+            SpriteBatch = sb;
+            GraphicsDevice = gd;
+            GraphicsDeviceManager = gdm;
+        }
+
+        public void Render(Texture2D texture, Vector2 position, Rectangle? sourceRectangle, Color color, float rotation, 
+            Vector2 origin, Vector2 scale, SpriteEffects effects, float layerDepth)
+        {
+            //if Globals.BegunRendering
+            //SpriteBatch.Begin();
+            SpriteBatch.Draw(texture, position, sourceRectangle, color, rotation, origin, scale, effects, layerDepth);
+            //SpriteBatch.End();
+        }
+
+
+        public void SetRenderProperties(SpriteSortMode ssm, BlendState bs, SamplerState ss, DepthStencilState dss, RasterizerState rs, Effect e, Matrix m)
+        {
+            SpriteBatch.Begin(ssm,bs,ss,dss,rs,e,m);
+        }
+
+        public void ClearRenderProperties()
+        {
+            SpriteBatch.End();
+        }
+
+    }
+}
