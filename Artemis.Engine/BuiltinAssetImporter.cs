446808274ad65edcfb143d33d3529306ef00faa4
Michael Ala <michaelfonzolo@gmail.com>
2015-11-13 09:30:11 -0500

    (Untested) Asset importers bare bones.

---
+++ Artemis.Engine/BuiltinAssetImporter.cs
@@ -0,0 +1,18 @@
+﻿using Artemis.Engine.Utilities;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Artemis.Engine
+{
+    /// <summary>
+    /// Builtin IAssetImporter for object types T already known by the ContentManager.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    internal class BuiltinAssetImporter<T> : AbstractAssetImporter
+    {
+        public override object ImportFrom(string filePath)
+        {
+            return AssetLoader.Content.Load<T>(
+                DirectoryUtils.MakeRelativePath(AssetLoader.ContentFolderName, filePath));
+        }
+    }
+}
---
+++ Artemis.Engine/Texture2DAssetImporter.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace Artemis.Engine
+{
+    /// <summary>
+    /// Builtin IAssetImporter for Texture2D objects.
+    /// </summary>
+    internal class Texture2DAssetImporter : BuiltinAssetImporter<Texture2D> { }
+}
